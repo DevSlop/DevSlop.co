78ff1cb0a6570db646072969b2e9329038097907
ShehacksPurple <shehacksPurple@github.com>
2019-06-24 11:13:42 -0400

    removed CSP against all advice

--- DevSlop/Startup.cs
+++ DevSlop/Startup.cs
@@ -96,11 +96,11 @@ namespace DevSlop
             }
 
             //Security headers make me happy
-           app.UseXContentTypeOptions();
+            app.UseXContentTypeOptions();
             app.UseReferrerPolicy(opts => opts.NoReferrer());
             app.UseXXssProtection(options => options.EnabledWithBlockMode());
             app.UseXfo(options => options.Deny());
-            app.UseCsp(opts => opts
+            /*app.UseCsp(opts => opts
              .BlockAllMixedContent()
              .StyleSources(s => s.Self())
              .StyleSources(s => s.UnsafeInline())
@@ -109,7 +109,7 @@ namespace DevSlop
              .FrameAncestors(s => s.Self())
              .ImageSources(s => s.Self())
              .ScriptSources(s => s.Self())
-             );
+             );*/
             //End Security Headers
 
 
